ec243cdc14d96a643c8d9351f75b242fd5163efe
井関 任 <20080028@isd001.info-sense.co.jp>
2019-01-16 14:10:48 +0000

    commit

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            let obj = memory.get(event.source.userId);
-            console.log("memory:" + obj.constructor.name);// TODO:
+            console.log("memory:" + memory.get(event.source.userId));// TODO:
 
             events_processed.push(function (event, bot) {
                 message_text = `毎度！ご注文は？`;
