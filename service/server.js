87c61e4abec8f220e77ef0da21fc4ae265d2a4a3
井関 任 <20080028@isd001.info-sense.co.jp>
2019-01-28 09:43:28 +0000

    commit

--- service/server.js
+++ service/server.js
@@ -35,9 +35,9 @@ module.exports = () => {
           // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
           if (event.type == "message" && event.message.type == "text"){
               let exec_client = await memory.get(event.source.userId);
-              console.log("event.message:" + event.message);// TODO:
+              console.log("event.message.text:" + event.message.text);// TODO:
 
-              if (event.message == "買い物リスト") {
+              if (event.message.text == "買い物リスト") {
                 console.log("exec_client:" + exec_client);// TODO:
 
                 if (exec_client == null || exec_client.constructor.name != "CreateShoppingList") {
@@ -46,7 +46,9 @@ module.exports = () => {
                   }
               }
 
-              console.log("exec_client2:" + exec_client.constructor.name);// TODO:
+              if (exec_client != null) {
+                console.log("exec_client2:" + exec_client.constructor.name);// TODO:
+              }
 
               events_processed.push(function (event, bot) {
                   let message_text = `毎度！ご注文は？`;
