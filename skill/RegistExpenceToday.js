ce649454e322941e88fb1ab698cdcc005174b33a
井関 任 <20080028@isd001.info-sense.co.jp>
2019-01-29 15:14:33 +0000

    commit

--- skill/RegistExpenceToday.js
+++ skill/RegistExpenceToday.js
@@ -53,7 +53,7 @@ class SkillRegistExpenceToday {
     }
 
     if (this.money != null && this.category != null) {
-      console.debug("today:" + db.getNowYMD); // TODO:
+      console.debug("today:" + db.getNowYMD()); // TODO:
       let return_message = `以下で登録します。\n${this.money}円\n${message_text}`;
       this.money = null;
       this.category = null;
