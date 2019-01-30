e544bf501f5893fce218df47c3580b731f475168
井関 任 <20080028@isd001.info-sense.co.jp>
2019-01-30 09:39:42 +0000

    commit

--- skill/RegistExpence.js
+++ skill/RegistExpence.js
@@ -4,6 +4,7 @@ const db = require("../service/postgres");
 
 class SkillRegistExpence {
   constructor() {
+    this.date = null;
     this.money = null;
     this.category = null;
   }
@@ -11,49 +12,49 @@ class SkillRegistExpence {
   async run(event, bot) {
     let message_text = event.message.text;
 
-    if (this.money == null) {
-      var regex = new RegExp(/^[0-9]+$/);
-      if (!regex.test(message_text)) {
-        return bot.replyMessage(event.replyToken, {
-          type: "text",
-          text: "金額を入力してください。"
-        });
-      } else {
-        this.money = message_text;
-        return bot.replyMessage(event.replyToken, {
-          type: "text",
-          text: "カテゴリは？",
-          quickReply: {
-            "items": [
-              // TODO:DBから取得したカテゴリをセットs
-              {
-                "type": "action",
-                "action": {
-                  "type": "message",
-                  "label": "ランチ",
-                  "text": "ランチ"
-                }
-              },
-              {
-                "type": "action",
-                "action": {
-                  "type": "message",
-                  "label": "漫画",
-                  "text": "漫画"
-                }
+    if (this.date == null && this.money == null) {
+      let registValue = [];
+      let str = message_text.split("\n");
+      str.forEach(element => {
+        registValue.push(element.split(":")[1]);
+      });
+      this.date = registValue[1];
+      this.money = registValue[2];
+
+      return bot.replyMessage(event.replyToken, {
+        type: "text",
+        text: "カテゴリは？",
+        quickReply: {
+          "items": [
+            // TODO:DBから取得したカテゴリをセットs
+            {
+              "type": "action",
+              "action": {
+                "type": "message",
+                "label": "ランチ",
+                "text": "ランチ"
               }
-            ]
-          }
-        });
-      }
+            },
+            {
+              "type": "action",
+              "action": {
+                "type": "message",
+                "label": "漫画",
+                "text": "漫画"
+              }
+            }
+          ]
+        }
+      });
     }
 
     if (this.category == null && message_text != "") {
       this.category = message_text;
     }
 
-    if (this.money != null && this.category != null) {
-      let return_message = `以下で登録します。\n${db.getNowYMD()}\n${this.money}円\n${message_text}`;
+    if (this.date != null && this.money != null && this.category != null) {
+      let return_message = `以下で登録します。\n${this.date}\n${this.money}円\n${message_text}`;
+      this.date = null;
       this.money = null;
       this.category = null;
       // TODO:DBに登録
