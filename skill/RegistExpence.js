2344430110f16f2bf0c7819f55f357e40b438180
井関 任 <20080028@isd001.info-sense.co.jp>
2019-01-31 03:56:24 +0000

    commit

--- skill/RegistExpence.js
+++ skill/RegistExpence.js
@@ -33,7 +33,7 @@ class SkillRegistExpence {
 
       // TODO:DBから取得したカテゴリをセット(最近使用したもの順にソート)
       let sqlParam = [event.source.userId];
-      let category_list = db.asyncSelect(sql_select_category, sqlParam);
+      let category_list = await db.asyncSelect(sql_select_category, sqlParam);
       category_list.rows.forEach(element => {
         replyMessage.quickReply.items.push({
           "type": "action",
