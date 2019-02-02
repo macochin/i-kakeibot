e7ff025a540029e70e0ec606502a9f7af1739c0e
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-02 13:02:45 +0000

    commit

--- service/server.js
+++ service/server.js
@@ -19,16 +19,16 @@ module.exports = () => {
         // 先行してLINE側にステータスコード200でレスポンスする。
         res.sendStatus(200);
 
-        if (process.env.SENDER_ID != undefined && process.env.SENDER_ID != event.source.userId) {
-            return bot.replyMessage(event.replyToken, {
-                type: "text",
-                text: "個人専用botとなっておりますので、申し訳ありませんがアクセスできません。"
-              });
-        }
-
         let events_processed = [];
 
         req.body.events.forEach(async (event) => {
+            if (process.env.SENDER_ID != undefined && process.env.SENDER_ID != event.source.userId) {
+                return bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "個人専用botとなっておりますので、申し訳ありませんがアクセスできません。"
+                });
+            }
+    
             if (event.type == "message" && event.message.type == "text") {
                 let exec_client = await memory.get(event.source.userId);
                 let skill_name = "";
