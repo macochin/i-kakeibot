f2367b5f72f1e6e99c9def302975ee0e27d8dac5
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-04 18:29:16 +0000

    commit

--- service/webapi.js
+++ service/webapi.js
@@ -8,20 +8,22 @@ const sql_select_expence_list = "select to_char(usedate, 'mm/dd') as usedate_md,
 
 module.exports = () => {
 
-  router.get('/expenceList', async function(req, res, next) {
-    let target_ym = req.params.ym;
+  router.get('/expenceList/:ym/:senderId', async function(req, res, next) {
+    console.debug("req.params.ym:" + req.params.ym);// TODO:
+    console.debug("req.params.senderId:" + req.params.senderId);// TODO:
 
-    let sqlParam = [process.env.SENDER_ID, target_ym];
+    let sqlParam = [process.env.SENDER_ID, req.params.ym];
     let expnece_list = await db.asyncSelect(sql_select_expence_list, sqlParam);
 
-    var ret = {};
-    expnece_list.rows.forEach(element => {
-      ret.push({
-        "usedate_md" : element.usedate_md,
-        "category" : element.category,
-        "money" : element.money
-      });
-    });
+    let ret = {};
+    Object.assign(ret, expnece_list.rows);
+    // expnece_list.rows.forEach(element => {
+    //   ret.push({
+    //     "usedate_md" : element.usedate_md,
+    //     "category" : element.category,
+    //     "money" : element.money
+    //   });
+    // });
 
     res.header('Content-Type', 'application/json; charset=utf-8')
     res.send(ret);
