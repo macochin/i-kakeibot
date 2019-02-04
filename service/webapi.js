83a2451dd837db0a2abf8da5f7f2f6b2b24d0ff8
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-04 18:47:31 +0000

    commit

--- service/webapi.js
+++ service/webapi.js
@@ -9,14 +9,12 @@ const sql_select_expence_list = "select to_char(usedate, 'mm/dd') as usedate_md,
 module.exports = () => {
 
   router.get('/expenceList/:ym/:senderId', async function(req, res, next) {
-    console.debug("req.params.ym:" + req.params.ym);// TODO:
-    console.debug("req.params.senderId:" + req.params.senderId);// TODO:
-
+    console.debug("process.env.SENDER_ID:" + process.env.SENDER_ID);// TODO:
     let sqlParam = [process.env.SENDER_ID, req.params.ym];
     let expnece_list = await db.asyncSelect(sql_select_expence_list, sqlParam);
 
     let ret = {};
-    Object.assign(ret, expnece_list);
+    Object.assign(ret, expnece_list.rows);
     // expnece_list.rows.forEach(element => {
     //   ret.push({
     //     "usedate_md" : element.usedate_md,
