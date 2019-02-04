cf5232f44184f7f3b94f7ca686970290b34bee00
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-04 18:49:02 +0000

    commit

--- service/webapi.js
+++ service/webapi.js
@@ -4,12 +4,11 @@ const express = require('express');
 const db = require("./postgres");
 const router = express.Router();
 
-const sql_select_expence_list = "select to_char(usedate, 'mm/dd') as usedate_md, category, money from accountBook where sender_id = $1 and to_char(usedate, 'yyyy/mm') = $2 order by usedate, update_date";
+const sql_select_expence_list = "select to_char(usedate, 'mm/dd') as usedate_md, category, money from accountBook where sender_id = $1 and to_char(usedate, 'yyyymm') = $2 order by usedate, update_date";
 
 module.exports = () => {
 
   router.get('/expenceList/:ym/:senderId', async function(req, res, next) {
-    console.debug("process.env.SENDER_ID:" + process.env.SENDER_ID);// TODO:
     let sqlParam = [process.env.SENDER_ID, req.params.ym];
     let expnece_list = await db.asyncSelect(sql_select_expence_list, sqlParam);
 
