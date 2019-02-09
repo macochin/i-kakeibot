e507e67ae58c401199ecbe3346974bf5cdaba2d5
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-09 21:38:10 +0000

    commit

--- skill/DispExpenceList.js
+++ skill/DispExpenceList.js
@@ -49,8 +49,8 @@ class SkillDispExpenceList {
           "type": "action",
           "action": {
             "type": "message",
-            "label": `${element.account_book_id}) ${element.usedate_md}\n${element.category}\n${element.money.toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円`,
-            "text": `${element.account_book_id}) ${element.usedate_md}\n${element.category}\n${element.money.toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円`
+            "label": `${element.account_book_id}) ${element.usedate_md}\n${element.category} ${element.money.toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円`,
+            "text": `${element.account_book_id}) ${element.usedate_md}\n${element.category} ${element.money.toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円`
           }
         });
       });
