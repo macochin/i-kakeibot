2b2cd54c2e7edbe2a0b0e4459774dc0fdac3b4d4
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-15 10:21:51 +0000

    commit

--- skill/UpdateShoppingList.js
+++ skill/UpdateShoppingList.js
@@ -11,14 +11,12 @@ class SkillUpdateShoppingList {
   async run(event, bot) {
     let message_text = event.message.text;
     let str = message_text.split("\n");
-    console.debug("str1:" + str);// TODO:
     str.shift();
-    console.debug("str2:" + str);// TODO:
 
     let str_shopping_id = "";
     let count = 0;
     str.forEach(element => {
-      if (count > 1) {
+      if (count > 0) {
         str_shopping_id += ",";
       }
       str_shopping_id += Number(element.split(":")[1]);
