4533366b5bcd0e2a3aaf8071abba53e8abd1eaa9
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-15 09:51:12 +0000

    commit

--- skill/UpdateShoppingList.js
+++ skill/UpdateShoppingList.js
@@ -13,9 +13,10 @@ class SkillUpdateShoppingList {
     let registValue = [];
     let str = message_text.split("\n");
     str.forEach(element => {
-      registValue.push(parseInt(element.split(":")[1]));
+      registValue.push(element.split(":")[1]);
     });
     registValue.shift();
+    registValue.map(str => parseInt(str, 10));
 
     let sqlParam_update = [db.getNowDate(), event.source.userId, registValue];
     await db.asyncUpdate(sql_update_shoppingList_true, sqlParam_update);
