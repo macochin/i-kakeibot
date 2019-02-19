d5ba3b617ccbb2d24dd870d5198c0707c29e7ec2
井関 任 <20080028@isd001.info-sense.co.jp>
2019-02-19 23:39:00 +0000

    commit

---
+++ create_table.sql
@@ -0,0 +1,22 @@
+DROP TABLE accountBook;
+CREATE TABLE accountBook
+(
+  account_book_id SERIAL NOT NULL,
+  sender_id TEXT NOT NULL,
+  useDate DATE NOT NULL,
+  money INTEGER NOT NULL,
+  category TEXT NOT NULL,
+  insert_date TIMESTAMP NOT NULL,
+  update_date TIMESTAMP NOT NULL
+);
+
+DROP TABLE shoppingList;
+CREATE TABLE shoppingList
+(
+  shopping_id SERIAL NOT NULL,
+  sender_id TEXT NOT NULL,
+  shopping_name TEXT NOT NULL,
+  plan_to_buy_flg BOOLEAN NOT NULL,
+  insert_date TIMESTAMP NOT NULL,
+  update_date TIMESTAMP NOT NULL
+);
