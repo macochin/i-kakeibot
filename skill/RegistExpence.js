6a31daf35de4e1f3316a8d021538a4bc9497aa4d
井関 任 <20080028@isd001.info-sense.co.jp>
2019-03-22 04:52:56 +0100

    commit

--- skill/RegistExpence.js
+++ skill/RegistExpence.js
@@ -12,106 +12,100 @@ class SkillRegistExpence {
   }
 
   async run(event, bot) {
-    try {
-      let message_text = event.message.text;
+    let message_text = event.message.text;
 
-      if (this.date == null && this.money == null) {
-        let registValue = [];
-        let str = message_text.split("\n");
-        str.forEach(element => {
-          registValue.push(element.split(":")[1]);
-        });
-        this.date = registValue[1];
-        this.money = registValue[2];
+    if (this.date == null && this.money == null) {
+      let registValue = [];
+      let str = message_text.split("\n");
+      str.forEach(element => {
+        registValue.push(element.split(":")[1]);
+      });
+      this.date = registValue[1];
+      this.money = registValue[2];
 
-        let replyMessage = {
-          type: "text",
-          text: "カテゴリは？",
-          quickReply: {
-            "items": []
+      let replyMessage = {
+        type: "text",
+        text: "カテゴリは？",
+        quickReply: {
+          "items": []
+        }
+      };
+
+      // DBから取得したカテゴリをセット(最近使用したもの順にソート)
+      let sqlParam = [event.source.userId];
+      let category_list = await db.asyncSelect(sql_select_category, sqlParam);
+      if (category_list.rows.length == 0) {
+        // DBから取得できない場有はデフォルト値をセット
+        replyMessage.quickReply.items.push({
+          "type": "action",
+          "action": {
+            "type": "message",
+            "label": `ランチ`,
+            "text": `ランチ`
           }
-        };
-
-        // DBから取得したカテゴリをセット(最近使用したもの順にソート)
-        let sqlParam = [event.source.userId];
-        let category_list = await db.asyncSelect(sql_select_category, sqlParam);
-        if (category_list.rows.length == 0) {
-          // DBから取得できない場有はデフォルト値をセット
-          replyMessage.quickReply.items.push({
-            "type": "action",
-            "action": {
-              "type": "message",
-              "label": `ランチ`,
-              "text": `ランチ`
-            }
-          });
-          replyMessage.quickReply.items.push({
-            "type": "action",
-            "action": {
-              "type": "message",
-              "label": `マンガ`,
-              "text": `マンガ`
-            }
-          });
-          replyMessage.quickReply.items.push({
-            "type": "action",
-            "action": {
-              "type": "message",
-              "label": `日用品`,
-              "text": `日用品`
-            }
-          });
-        }
-
-        // TODO:Quick Replyの数に制限あり？
-        for (let index = 0; index < 10; index++) {
-          replyMessage.quickReply.items.push({
-            "type": "action",
-            "action": {
-              "type": "message",
-              "label": `${category_list.rows[index].category}`,
-              "text": `${category_list.rows[index].category}`
-            }
-          });
-        }
-
-        // category_list.rows.forEach(element => {
-        //   replyMessage.quickReply.items.push({
-        //     "type": "action",
-        //     "action": {
-        //       "type": "message",
-        //       "label": `${element.category}`,
-        //       "text": `${element.category}`
-        //     }
-        //   });
-        // });
-
-        // TODO:14
-        return bot.replyMessage(event.replyToken, replyMessage);
-      }
-
-      if (this.category == null && message_text != "") {
-        this.category = message_text;
-      }
-
-      if (this.date != null && this.money != null && this.category != null) {
-        let sqlParam = [event.source.userId, this.date.replace(/\//g, '-'), this.money, this.category, db.getNowDate(), db.getNowDate()];
-        await db.asyncUpdate(sql_insert_expence, sqlParam);
-
-        let return_message = `以下で登録します。\n${this.date.replace(/-/g, '/')}\n${this.money.replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円\n${this.category}`;
-        this.date = null;
-        this.money = null;
-        this.category = null;
-
-        return bot.replyMessage(event.replyToken, {
-          type: "text",
-          text: return_message
+        });
+        replyMessage.quickReply.items.push({
+          "type": "action",
+          "action": {
+            "type": "message",
+            "label": `マンガ`,
+            "text": `マンガ`
+          }
+        });
+        replyMessage.quickReply.items.push({
+          "type": "action",
+          "action": {
+            "type": "message",
+            "label": `日用品`,
+            "text": `日用品`
+          }
         });
       }
-    } catch (e) {
-      console.debug("error:" + e);// TODO:
-      console.error(e);
-      throw e;
+
+      // TODO:Quick Replyの数に制限あり？
+      for (let index = 0; index < 10; index++) {
+        replyMessage.quickReply.items.push({
+          "type": "action",
+          "action": {
+            "type": "message",
+            "label": `${category_list.rows[index].category}`,
+            "text": `${category_list.rows[index].category}`
+          }
+        });
+      }
+
+      // category_list.rows.forEach(element => {
+      //   replyMessage.quickReply.items.push({
+      //     "type": "action",
+      //     "action": {
+      //       "type": "message",
+      //       "label": `${element.category}`,
+      //       "text": `${element.category}`
+      //     }
+      //   });
+      // });
+
+      // TODO:14
+      return bot.replyMessage(event.replyToken, replyMessage);
+    }
+
+    if (this.category == null && message_text != "") {
+      this.category = message_text;
+    }
+
+    if (this.date != null && this.money != null && this.category != null) {
+      let sqlParam = [event.source.userId, this.date.replace(/\//g, '-'), this.money, this.category, db.getNowDate(), db.getNowDate()];
+      await db.asyncUpdate(sql_insert_expence, sqlParam);
+
+      let return_message = `以下で登録します。\n${this.date.replace(/-/g, '/')}\n${this.money.replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}円\n${this.category}`;
+      this.date = null;
+      this.money = null;
+      this.category = null;
+
+      return bot.replyMessage(event.replyToken, {
+        type: "text",
+        text: return_message
+      });
     }
   }
 }
