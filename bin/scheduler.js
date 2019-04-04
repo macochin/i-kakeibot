4194cedbce5120eb2ca295ebda6be70afda3fc75
井関 任 <20080028@isd001.info-sense.co.jp>
2019-04-04 08:49:29 +0100

    commit

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -1,5 +1,11 @@
 'use strict';
 
 const db = require("../service/postgres");
+const sql_delete_expence = "delete from accountBook where useDate < $1";
 
-// TODO:古いデータを削除(3か月以上前？)
+// 古いデータを削除(3か月以上前)
+let dt = db.getNowDate();
+dt.setMonth(dt.getMonth() - 3);
+
+let sqlParam = [dt];
+db.asyncUpdate(sql_delete_expence, sqlParam);
