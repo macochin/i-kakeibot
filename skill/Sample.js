8fb2de4083d8ff75c944df2b728b131bf6883d0f
井関 任 <20080028@isd001.info-sense.co.jp>
2019-07-11 11:39:21 +0100

    commit

--- skill/Sample.js
+++ skill/Sample.js
@@ -1,6 +1,45 @@
 "use strict";
+
+const GoogleSpreadsheet = require('google-spreadsheet');
+
 class SkillSample {
   async run(event, bot) {
+    let book = new GoogleSpreadsheet(process.env.SPREDSHEET_ID);
+    let creds = {
+      client_email: process.env.GOOGLE_SPREAD_CLIENT_EMAIL
+      , private_key: process.env.GOOGLE_SPREAD_PRIVATE_KEY
+    }
+
+    var spreadsheet; //スプレッドシート
+
+    //認証を通しスプレッドシートの情報取得して外部スコープの変数に保存
+    book.useServiceAccountAuth(creds, function(error) {
+      if (error !== null) {
+        throw new Error(error);
+      }
+      book.getInfo(function(error, data) {
+        if (error !== null) {
+          throw new Error(error);
+        }
+        spreadsheet = data;
+      });
+    });
+
+    for (let rcnt1 in spreadsheet.worksheets) {
+      spreadsheet.worksheets[rcnt1].getRows({
+        offset: 1, //何も指定しなければ2行目から読み込むので1を指定すると3行目から読み込む
+        limit: 20, //途中で空白行が現れれば20行以下でも読み込み中止
+      }, function(error, rows) {
+        if (error !== null) {
+          throw new Error(error);
+        }
+        // TODO:書き込み
+        rows[0].colname = 'new val';
+        rows[0].save();
+      });
+    }
+
+    // TODO:debug
     return bot.replyMessage(event.replyToken, {
       type: "text",
       text: "テスト中..."
