fc00c0d16d7f322a5834c94e835fab867a5d2d9d
井関 任 <20080028@isd001.info-sense.co.jp>
2019-08-29 12:01:20 +0100

    commit

--- service/gas.js
+++ service/gas.js
@@ -14,117 +14,37 @@ const TOKEN_PATH = TOKEN_DIR + 'i-kakeibot.json';
 
 class ServiceGas {
   async asyncCreateSheet(userId) {
-    this.authorize(userId);
+    let token = await this.authorize(userId);
+    if (token == null) {
+      let oauth2Client = new google.auth.OAuth2(
+        process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, REDIRECT_URL);
+      token = await this.getNewToken(oauth2Client, userId)
+      await this.storeToken(token);
+    }
+    this.createSheet(oauth2Client, userId);
   }
 
   authorize(userId) {
-    // TODO:エラーになる。。。
-    let oauth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, REDIRECT_URL);
+    // let oauth2Client = new google.auth.OAuth2(
+    //   process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, REDIRECT_URL);
 
-    fs.readFile(TOKEN_PATH, function (err, token) {
-      if (err) {
-        // this.getNewToken(oauth2Client, userId);
-        let authUrl = oauth2Client.generateAuthUrl({
-          access_type: 'offline',
-          scope: SCOPES
-        });
-        console.log('Authorize this app by visiting this url: ', authUrl);
-        let rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        });
-        rl.question('Enter the code from that page here: ', function (code) {
-          rl.close();
-          oauth2Client.getToken(code, function (err, token) {
-            if (err) {
-              console.log('Error while trying to retrieve access token', err);
-              return;
-            }
-            oauth2Client.credentials = token;
-            // this.storeToken(token);
-            // this.createSheet(oauth2Client, userId);
+    // fs.readFile(TOKEN_PATH, function (err, token) {
+    //   if (err) {
+    //     this.getNewToken(oauth2Client, userId);
+    //   } else {
+    //     oauth2Client.credentials = JSON.parse(token);
+    //     this.createSheet(oauth2Client, userId);
+    //   }
+    // });
 
-            try {
-              fs.mkdirSync(TOKEN_DIR);
-            } catch (err) {
-              if (err.code != 'EEXIST') {
-                throw err;
-              }
-            }
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-            console.log('Token stored to ' + TOKEN_PATH);
-        
-            let scriptId = '1ol9N5RK7MfElClbMdxcHZtazE5pJfresDOck5UWUIC1MlRko_JLYqhJZ';
-            let script = google.script('v1');
-        
-            script.scripts.run({
-              auth: auth,
-              resource: {
-                function: 'createSpreadsheet'
-              },
-              scriptId: scriptId
-            }, function (err, resp) {
-              if (err) {
-                console.log('The API returned an error: ' + err);
-                return;
-              }
-              if (resp.error) {
-                let error = resp.error.details[0];
-                console.log('Script error message: ' + error.errorMessage);
-                console.log('Script error stacktrace:');
-        
-                if (error.scriptStackTraceElements) {
-                  for (let i = 0; i < error.scriptStackTraceElements.length; i++) {
-                    let trace = error.scriptStackTraceElements[i];
-                    console.log('\t%s: %s', trace.function, trace.lineNumber);
-                  }
-                }
-              } else {
-                let folderSet = resp.response.result;
-                console.log(resp.response.result);
-              }
-        
-            });
-          });
-        });
-    
-      } else {
-        oauth2Client.credentials = JSON.parse(token);
-        // this.createSheet(oauth2Client, userId);
-        let scriptId = '1ol9N5RK7MfElClbMdxcHZtazE5pJfresDOck5UWUIC1MlRko_JLYqhJZ';
-        let script = google.script('v1');
-    
-        script.scripts.run({
-          auth: auth,
-          resource: {
-            function: 'createSpreadsheet'
-          },
-          scriptId: scriptId
-        }, function (err, resp) {
-          if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-          }
-          if (resp.error) {
-            let error = resp.error.details[0];
-            console.log('Script error message: ' + error.errorMessage);
-            console.log('Script error stacktrace:');
-    
-            if (error.scriptStackTraceElements) {
-              for (let i = 0; i < error.scriptStackTraceElements.length; i++) {
-                let trace = error.scriptStackTraceElements[i];
-                console.log('\t%s: %s', trace.function, trace.lineNumber);
-              }
-            }
-          } else {
-            let folderSet = resp.response.result;
-            console.log(resp.response.result);
-          }
-    
-        });
-    
-      }
+    return new Promise((resolve, reject) => {
+      fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+          return resolve();
+        } else {
+          return resolve(token)
+        }
+      });
     });
   }
 
@@ -163,39 +83,79 @@ class ServiceGas {
   }
 
   getNewToken(oauth2Client, userId) {
-    let authUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES
-    });
-    console.log('Authorize this app by visiting this url: ', authUrl);
-    let rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.question('Enter the code from that page here: ', function (code) {
-      rl.close();
-      oauth2Client.getToken(code, function (err, token) {
-        if (err) {
-          console.log('Error while trying to retrieve access token', err);
-          return;
-        }
-        oauth2Client.credentials = token;
-        this.storeToken(token);
-        this.createSheet(oauth2Client, userId);
+    // let authUrl = oauth2Client.generateAuthUrl({
+    //   access_type: 'offline',
+    //   scope: SCOPES
+    // });
+
+    // console.log('Authorize this app by visiting this url: ', authUrl);
+    // let rl = readline.createInterface({
+    //   input: process.stdin,
+    //   output: process.stdout
+    // });
+
+    // rl.question('Enter the code from that page here: ', function (code) {
+    //   rl.close();
+    //   oauth2Client.getToken(code, function (err, token) {
+    //     if (err) {
+    //       console.log('Error while trying to retrieve access token', err);
+    //       return;
+    //     }
+    //     oauth2Client.credentials = token;
+    //     this.storeToken(token);
+    //     this.createSheet(oauth2Client, userId);
+    //   });
+    // });
+
+    return new Promise((resolve, reject) => {
+      let authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+      });
+  
+      console.log('Authorize this app by visiting this url: ', authUrl);
+      let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+  
+      rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+          if (err) {
+            console.log('Error while trying to retrieve access token', err);
+            return;
+          }
+          return resolve(token);
+        });
       });
     });
   }
 
   storeToken(token) {
-    try {
-      fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-      if (err.code != 'EEXIST') {
-        throw err;
+    // try {
+    //   fs.mkdirSync(TOKEN_DIR);
+    // } catch (err) {
+    //   if (err.code != 'EXIST') {
+    //     throw err;
+    //   }
+    // }
+    // fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    // console.log('Token stored to ' + TOKEN_PATH);
+
+    return new Promise((resolve, reject) => {
+      try {
+        fs.mkdirSync(TOKEN_DIR);
+      } catch (err) {
+        if (err.code != 'EXIST') {
+          throw err;
+          return reject();
+        }
       }
-    }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_PATH);
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+      console.log('Token stored to ' + TOKEN_PATH);
+      return resolve();
+    });
   }
 }
 
