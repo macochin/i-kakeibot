5455895298191854bf0346decfee6e9fb674afd2
井関 任 <20080028@isd001.info-sense.co.jp>
2020-01-31 09:30:27 +0000

    commit

--- service/webapi.js
+++ service/webapi.js
@@ -6,6 +6,8 @@ const db = require("./postgres");
 
 const router = express.Router();
 
+const sql_select_expence_list = "select to_char(usedate, 'mm/dd') as usedate, category, money from accountBook where sender_id = $1 and to_char(usedate, 'yyyymm') = $2 order by usedate, update_date";
+
 module.exports = () => {
 
   router.get('/expenceList/:ym/:senderId', async function(req, res, next) {
