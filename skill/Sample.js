26ee69314f9f363bc4d352b75846553fd2eeb61f
macochin <m.i.nority.dev@gmail.com>
2020-10-09 06:29:59 +0100

    commit

--- skill/Sample.js
+++ skill/Sample.js
@@ -8,7 +8,10 @@ class SkillSample {
 
   async run(event, bot) {
     let sheet = await expence.asyncSearchSheet(process.env.SENDER_ID);
-    return bot.replyMessage(event.replyToken, "sample成功:" + sheet.title);
+    return bot.replyMessage(event.replyToken, {
+      type: "text",
+      text: "sample成功:" + sheet.title
+    });
   }
 }
 
