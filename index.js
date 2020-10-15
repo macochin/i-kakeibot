3e979a4e3d9ae75258939e63fd7aaa76f7fad583
macochin <m.i.nority.dev@gmail.com>
2020-10-15 07:20:25 +0100

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const express = require("express");
 const server = express();
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const serviceServer = require("./service/server")
 const webapi = require("./service/webapi");
@@ -10,8 +11,18 @@ const auth = require("./service/googleOauth2");
 // -----------------------------------------------------------------------------
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
-server.set("view engine", "ejs");
 server.use(express.static('public'));
+server.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie:{
+  httpOnly: true,
+  secure: false,
+  maxage: 1000 * 60 * 5
+  }
+}));
+server.set("view engine", "ejs");
 server.use("/webhook", serviceServer());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: false}));
