bf74211f1ba58a7116bedf5c09a58d3f13ee2c74
macochin <m.i.nority.dev@gmail.com>
2020-11-10 04:21:38 +0000

    commit

--- service/userInfo.js
+++ service/userInfo.js
@@ -10,9 +10,9 @@ var resource = process.env.RESOURCE_USER_INFO;
 const resource_spreadsheet = "spread";
 const resource_postgres = "postgres";
 
-const sql_select_userInfo = "select userId, sheetId from userInfo where userId = $1";
-const sql_insert_userInfo = "insert into userInfo (userId, sheetId) VALUES ($1, $2)";
-const sql_update_sheetId = "update userInfo set sheetId = $1 where userId = $2";
+const sql_select_userInfo = "select userid, sheetid from userInfo where userid = $1";
+const sql_insert_userInfo = "insert into userInfo (userid, sheetid) VALUES ($1, $2)";
+const sql_update_sheetId = "update userInfo set sheetid = $1 where userid = $2";
 
 class userInfo {
   constructor() {
@@ -29,14 +29,11 @@ class userInfo {
       let rows = await spreadsheet.getRows(master_spread_id, "マスタ");
       row = await spreadsheet.searchRow(rows, crypt_userId, "userId");
     } else if (resource == resource_postgres) {
-      // TODO:
       let sqlParam = [crypt_userId];
       let ret = await pg.asyncSelect(sql_select_userInfo, sqlParam);
-      console.debug("Object.getOwnPropertyNames(ret.rows):" + Object.getOwnPropertyNames(ret.rows));// TODO:
-      console.debug("Object.getOwnPropertyNames(ret.rows):" + Object.getOwnPropertyNames(ret.rows[0]));// TODO:
       if (ret.rows.length == 1) {
-        row.userId = ret.rows[0].userId;
-        row.sheetId = ret.rows[0].sheetId;
+        row.userId = ret.rows[0].userid;
+        row.sheetId = ret.rows[0].sheetid;
       }
     }
     return row;
