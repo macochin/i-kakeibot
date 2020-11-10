3411847920c4ea808d9aa99636118af2698e8020
macochin <m.i.nority.dev@gmail.com>
2020-11-10 02:47:05 +0000

    commit

--- service/userInfo.js
+++ service/userInfo.js
@@ -2,12 +2,17 @@
 
 const spreadsheet = require("../common/Spreadsheet");
 const crypto = require('../common/crypto');
+const pg = require("../common/postgres");
 
 const master_spread_id = process.env.MASTER_SPREAD_ID;
 
 var resource = process.env.RESOURCE_USER_INFO;
 const resource_spreadsheet = "spread";
 const resource_postgres = "postgres";
+
+const sql_select_userInfo = "select userId, sheetId from userInfo where userId = $1";
+const sql_insert_userInfo = "insert into userInfo (userId, sheetId) VALUES ($1, $2, $3)";
+const sql_update_sheetId = "update userInfo set sheetId = $1 where userId = $2";
 
 class userInfo {
   constructor() {
@@ -25,20 +30,19 @@ class userInfo {
       row = await spreadsheet.searchRow(rows, crypt_userId, "userId");
     } else if (resource == resource_postgres) {
       // TODO:
+      let sqlParam = [crypt_userId];
+      let ret = await pg.asyncSelect(sql_select_userInfo, sqlParam);
+      if (ret.rows.length == 1) {
+        row.userId = ret.rows[0].userId;
+        row.sheetId = ret.rows[0].sheetId;
+      }
     }
     return row;
   }
 
   async asyncSearchUserSheetId(userId) {
-    let sheetId;
-    if (resource == resource_spreadsheet) {
-      let row = await this.asyncSearchUserRow(userId);
-      sheetId = row.sheetId;
-    } else if (resource == resource_postgres) {
-      // TODO:
-    }
-
-    return sheetId;
+    let row = await this.asyncSearchUserRow(userId);
+    return row.sheetId;
   }
 
   async asyncInsertMasterInfo(userId, sheetId) {
@@ -51,6 +55,8 @@ class userInfo {
       spreadsheet.addRow(master_spread_id, "マスタ", row);
     } else if (resource == resource_postgres) {
       // TODO:
+      let sqlParam = [crypt_userId, sheetId];
+      await pg.asyncUpdate(sql_insert_userInfo, sqlParam);
     }
   }
 
@@ -63,6 +69,8 @@ class userInfo {
       await row.save();
     } else if (resource == resource_postgres) {
       // TODO:
+      let sqlParam = [sheetId, crypt_userId];
+      await pg.asyncUpdate(sql_update_sheetId, sqlParam);
     }
   }
 
