8508ee9866c3310ba7b42db5a606efe51817877c
macochin <m.i.nority.dev@gmail.com>
2021-03-22 07:29:52 +0100

    commit

--- common/postgres2.js
+++ common/postgres2.js
@@ -4,7 +4,8 @@ const pg = require('pg');
 const conString = process.env.DATABASE_URL;
 const pg = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  // ssl: true
+  ssl: { rejectUnauthorized: false }
 });
 
 class Postgres {
